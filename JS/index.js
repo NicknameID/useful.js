7dc56722fcded2d7b0551d2c5a3e4dde8820cf42
Huang Tianyu <huangtianyu_hty@163.com>
2017-11-25 22:37:01 +0800

    为index.js的每个方法写上注释，方便日后能够快速的了解每个方法的作用

--- JS/index.js
+++ JS/index.js
@@ -25,26 +25,26 @@ import EventPool from "./EventPool";
 const M = (function () {
   let M = {};
   //封装方法
-  M.getStyle = getStyle;
-  M.getWindow = getWindow;
-  M.isArrayLike = isArrayLike;
-  M.getByClass = getByClass;
-  M.getEvent = getEvent;
-  M.addListener = addListener;
-  M.removeListener = removeListener;
-  M.drag = drag;
-  M.startMove = startMove;
-  M.rollMouse = rollMouse;
-  M.Ajax = Ajax;
-  M.AjaxPromise = AjaxPromise;
-  M.setCookie = setCookie;
-  M.getCookie = getCookie;
-  M.delCookie = delCookie;
-  M.Slider = Slider;
-  M.inherit = inherit;
-  M.getScreenXY = getScreenXY;
-  M.NodeWalker = walker;
-  M.EventPool = EventPool;
+  M.getStyle = getStyle; //通过DOM节点获取该节点处的CSS计算后的样式，当传入参3时，是设置新的样式
+  M.getWindow = getWindow; //兼容性的获取窗口（可视窗口）的宽高
+  M.isArrayLike = isArrayLike; //判断是不是数组
+  M.getByClass = getByClass; //通过类名来获取DOM节点，主要是兼容IE
+  M.getEvent = getEvent; //没啥作用，就一句话  "oEv = ev || window.event"
+  M.addListener = addListener;  //参考下者
+  M.removeListener = removeListener;  //具有IE兼容性的移除事件监听，参数与原生是一样的
+  M.drag = drag; //基于下面那个运动函数的自定义鼠标的拖拽事件
+  M.startMove = startMove; //比较基础的运动框架函数，类似jQuery的animate，不过推荐使用jquery的运动比较好
+  M.rollMouse = rollMouse; //鼠标滚动事件
+  M.Ajax = Ajax; //普通的ajax函数封装
+  M.AjaxPromise = AjaxPromise; //基于Promise的ajax，没啥用低版本浏览器不兼容
+  M.setCookie = setCookie; //设置cookie
+  M.getCookie = getCookie; //获取cookie
+  M.delCookie = delCookie; //删除cookie
+  M.Slider = Slider; //没啥用的轮播图插件
+  M.inherit = inherit; //面向对象编程时，用于两个构造函数之间的继承
+  M.getScreenXY = getScreenXY; //
+  M.NodeWalker = walker;   //DOM节点遍历器
+  M.EventPool = EventPool; //全局自定义事件机制，有register、dispatch、remove、namespace方法
 
 //---------------------
   return M;
